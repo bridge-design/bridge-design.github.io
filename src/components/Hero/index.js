8c399bc265ba65056254580371a57d7a43910c7d
Varya Stepanova <mail@varya.me>
2020-06-23 15:59:39 +0300

    Dummy Hero component

---
+++ src/components/Hero/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { colors } from '../DesignTokens';
+
+const Hero = () => (
+  <h1>I am hero</h1>
+);
+
+export { Hero };
