68b4b56a1b23cfc5f5ae86b5dbf933f04f9da7f4
Varya Stepanova <mail@varya.me>
2020-06-23 22:41:17 +0300

    Use fonts in Hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { colors } from '../DesignTokens';
+import { colors, fonts } from '../DesignTokens';
 
 import BridgeIcon from '../../assets/bridge.svg'
 
@@ -18,11 +18,13 @@ export const Logo = styled(BridgeIcon)`
 
 export const Name = styled.span`
   font-size: 40px;
-  font-family: 'Seaweed Script', cursive;
+  font-family: ${fonts.logo};
 `;
 
 export const Slogan = styled.span`
   font-size: 30px;
+  font-family: ${fonts.regular};
+  font-weight: 200;
 `;
 
 const Hero = () => (
