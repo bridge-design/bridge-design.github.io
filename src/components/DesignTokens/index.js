3fe3cb15c7222daa5446e425bec457e9345eaf68
Varya Stepanova <mail@varya.me>
2020-06-28 21:54:48 +0300

    New colors

--- src/components/DesignTokens/index.js
+++ src/components/DesignTokens/index.js
@@ -1,28 +1,85 @@
-export const colors = {
+let colors = {
+
+  blue100: {
+    value: '#E2ECFC'
+  },
+
+  blue200: {
+    value: '#B7D0F9'
+  },
+
+  blue300: {
+    value: '#8CB4F5'
+  },
+
+  blue400: {
+    value: '#6098F1'
+  },
+
+  blue500: {
+    value: '#357DED',
+  },
+
+  blue600: {
+    value: '#125CD2'
+  },
+
+  blue700: {
+    value: '#0F4BAC'
+  },
+
+  blue800: {
+    value: '#0A3273'
+  },
+
+  blue900: {
+    value: '#051939'
+  },
+
+  pink: {
+    value: '#FF78BA'
+  },
+
+  white: {
+    value: '#FFFFFF'
+  },
+
+  black: {
+    value: '#000000'
+  },
+
+};
+
+Object.assign(colors, {
 
   primary: {
-    value: '#384259',
+    value: colors.blue400.value,
     meaning: 'Main brand color',
   },
 
   hightlight: {
-    value: '#f73859',
+    value: colors.pink.value,
     meaning: 'Highlights',
   },
 
   secondary: {
-    value: '#7ac7c4',
+    value: colors.blue700.value,
     meaning: 'Color for secondary content and elements',
   },
 
   shadow: {
-    value: '#c4edde',
+    value: colors.blue100.value,
     meaning: 'Disabled or not active things',
   }
 
-};
+});
 
-export const fonts = {
+const fonts = {
   logo: '"Seaweed Script", cursive',
   regular: '"Catamaran", sans-serif',
+}
+
+export {
+  colors,
+  fonts,
 }
