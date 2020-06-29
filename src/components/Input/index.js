b136a528275122b2e0cdf018703a86405a3dfcd7
Varya Stepanova <mail@varya.me>
2020-06-29 21:53:53 +0300

    Correct font for Input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -6,15 +6,16 @@ import { colors, fonts } from '../DesignTokens';
 
 const InputContainer = styled(InputAntd)`
   color: ${colors.black.value};
-  font-size: 16px;
-  font-family: ${fonts.regular};
+  font-size: 14px;
+  font-family: ${fonts.brand};
   font-weight: 400;
   line-height: 2em;
-  padding-left: 0.5em;
+  padding-left: 16px;
+  border: none;
 
   &::placeholder {
-    font-size: 16px;
-    font-family: ${fonts.regular};
+    font-size: 14px;
+    font-family: ${fonts.brand};
     font-weight: 400;
     color: ${colors.blue300.value};
   }
