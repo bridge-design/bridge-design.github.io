cbb3e465abcab1fd668b848e04074e82475ddf41
Varya Stepanova <mail@varya.me>
2020-06-29 21:54:04 +0300

    Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,30 +1,24 @@
+import React from 'react';
 import styled from 'styled-components';
+import { Button as ButtonAntd } from 'antd';
+import "antd/lib/button/style";
 
-const Button = styled('button')`
-  padding: 4px 12px;
-  margin: 0;
-  background: none;
-  border: none;
-  border: 2px solid #663399;
-  border-radius: 2px;
-  color: #663399;
-  font-size: 14px;
+import { colors, fonts } from '../DesignTokens';
+
+const ButtonContainer = styled(ButtonAntd)`
+  font-size: 14x;
+  font-family: ${fonts.brand};
   font-weight: 500;
-  letter-spacing: 0.4px;
-  line-height: 24px;
-  text-transform: uppercase;
-  transition: all ease-in-out 0.2s;
-  transition-property: color, background-color;
-
-  &:hover,
-  &:focus {
-    color: #fff;
-    background-color: #663399;
-  }
+  height: 3em;
+  color: ${colors.white.value};
+  background-color: ${props => props.type === "primary" ? colors.pink.value : colors.blue700.value};
+  border-radius: 4px;
+  border: none;
+  padding: 0 16px;
 `;
 
-Button.defaultProps = {
-  type: 'button',
-};
+const Button = (props) => (
+  <ButtonContainer {...props} />
+);
 
 export { Button };
